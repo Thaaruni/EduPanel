c204b1975c7df2da3ccb5c8891433722456c859e
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-05-01 22:32:09 +0530

    Integrate ModelMapper for mapping DTOs to entities

--- src/main/java/eduPanel/WebAppConfig.java
+++ src/main/java/eduPanel/WebAppConfig.java
@@ -1,20 +1,23 @@
 package eduPanel;
 
 
-import eduPanel.api.LecturerHttpController;
 import eduPanel.converter.LecturerTypeConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class WebAppConfig implements WebMvcConfigurer  {
+
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new LecturerTypeConverter());
